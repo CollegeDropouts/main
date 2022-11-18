b5eb79c4fbbc7bffa4aefd00c6c4e1132f1b8d3b
graygroves <63022132+graygroves@users.noreply.github.com>
2022-11-18 13:50:14 -0700

    Updates grovesGray.js
    fixed question mark when there should have been a period

--- website-template/src/pages/grayGroves.js
+++ website-template/src/pages/grayGroves.js
@@ -6,7 +6,7 @@ export default function examplePage() {
 
         <main>
           <div className='underline'>
-            <h1 className='title'>Hello, my name is Gray Groves?</h1>
+            <h1 className='title'>Hello, my name is Gray Groves.</h1>
           </div>
           <img
             className='pic'
